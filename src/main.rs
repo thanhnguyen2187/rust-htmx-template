82a94ec954954fe65af4936d2ed0840a131027b6
Thanh Nguyen <thanhnguyen2187@gmail.com>
2026-03-27 01:32:02 +0700

    fix: correct handler usage

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,7 @@ use crate::templates::{
     page_create_todo, page_default_todo, page_delete_todo, page_edit_todo, page_home, page_login,
     page_login_success, page_save_todo, page_toggle_todo, page_unimplemented,
 };
-use crate::views::{
-    create_todo_handler, edit_todo_handler, hello_handler, home_handler, toggle_todo_handler,
-};
+use crate::views::{create_todo_handler, edit_todo_handler, hello_handler, home_handler, save_todo_handler, toggle_todo_handler};
 use auth::page_login_check;
 use axum::routing::{delete, post};
 use axum::{Router, routing::get};
@@ -60,7 +58,7 @@ async fn main() -> Result<()> {
         .route("/toggle/{todo_id}", post(toggle_todo_handler))
         // .route("/default/{todo_id}", post(page_default_todo))
         .route("/edit/{todo_id}", post(edit_todo_handler))
-        .route("/save/{todo_id}", post(page_save_todo))
+        .route("/save/{todo_id}", post(save_todo_handler))
         .route("/create", post(create_todo_handler))
         // .route("/delete/{todo_id}", delete(page_delete_todo))
         .with_state(Arc::new(Mutex::new(AppState { conn })))
