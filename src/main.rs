0863742106bd446c80f0d9379b3aab636f8f3342
Thanh Nguyen <thanhnguyen2187@gmail.com>
2026-03-26 08:42:35 +0700

    chore: remove redundant imports

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,15 @@ use crate::templates::{
 };
 use auth::page_login_check;
 use axum::routing::{delete, post};
-use axum::{routing::get, Router};
+use axum::{Router, routing::get};
 use axum_login::tower_sessions::cookie::time::Duration;
 use axum_login::tower_sessions::{Expiry, MemoryStore, SessionManagerLayer};
-use axum_login::{login_required, AuthManagerLayerBuilder};
+use axum_login::{AuthManagerLayerBuilder, login_required};
 use diesel::SqliteConnection;
 use diesel_migrations::MigrationHarness;
 use dotenvy::dotenv;
 use snafu::ResultExt;
-use std::ops::Mul;
 use std::sync::{Arc, Mutex};
-use std::time;
 use tower_http::services::ServeDir;
 use tower_livereload::LiveReloadLayer;
 
@@ -37,8 +35,6 @@ async fn main() -> Result<()> {
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let mut conn = db::establish_connection(&db_url)
         .with_whatever_context(|err| format!("Failed to connect to in-memory database {err}"))?;
-    // TODO: resolve this issue in a more thorough way as the error is not very
-    //       descriptive
     conn.run_pending_migrations(MIGRATIONS)
         .map_err(|_| Error::DatabaseMigration {})?;
 
