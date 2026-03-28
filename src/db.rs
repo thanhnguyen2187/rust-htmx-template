5b1e4fc14a27039b810ceaa07459c3e6948f1faf
Thanh Nguyen <thanhnguyen2187@gmail.com>
2026-03-28 09:40:10 +0700

    fix: correct `eq` to `equals` of SeaQuery

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,5 @@
 use crate::err::Result;
-use sea_query::{Expr, Query, SqliteQueryBuilder};
+use sea_query::{Expr, ExprTrait, Iden, Query, SqliteQueryBuilder};
 use sea_query_rusqlite::RusqliteBinder;
 use serde::{Deserialize, Serialize};
 use snafu::ResultExt;
@@ -66,17 +66,17 @@ pub fn update_todo(conn: &mut rusqlite::Connection, item: &Todo) -> Result<usize
         .table("todos")
         .value("title", item.title.clone())
         .value("completed", item.completed)
-        .and_where(Expr::col("id").eq(item.id.as_str()))
+        .and_where(Expr::col("id").equals(item.id.clone()))
         .build_rusqlite(SqliteQueryBuilder);
 
     let mut stmt = conn.prepare_cached(sql.as_str())?;
     Ok(stmt.execute(&*values.as_params())?)
 }
 
-pub fn delete_todo(conn: &mut rusqlite::Connection, todo_id: &String) -> Result<usize> {
+pub fn delete_todo(conn: &mut rusqlite::Connection, todo_id: &str) -> Result<usize> {
     let (sql, values) = Query::delete()
         .from_table("todos")
-        .and_where(Expr::col("id").eq(todo_id.as_str()))
+        .and_where(Expr::col("id").equals(todo_id.to_string()))
         .build_rusqlite(SqliteQueryBuilder);
 
     let mut stmt = conn.prepare_cached(sql.as_str())?;
@@ -89,18 +89,18 @@ pub fn toggle_todo(conn: &mut rusqlite::Connection, item_id: &String) -> Result<
     let (sql, values) = Query::update()
         .table("todos")
         .value("completed", !todo.completed)
-        .and_where(Expr::col("id").eq(item_id.as_str()))
+        .and_where(Expr::col("id").equals(item_id.to_string()))
         .build_rusqlite(SqliteQueryBuilder);
 
     let mut stmt = conn.prepare_cached(sql.as_str())?;
     Ok(stmt.execute(&*values.as_params())?)
 }
 
-pub fn read_todo(conn: &mut rusqlite::Connection, todo_id: &String) -> Result<Todo> {
+pub fn read_todo(conn: &mut rusqlite::Connection, todo_id: &str) -> Result<Todo> {
     let (sql, values) = Query::select()
         .columns(["id", "title", "completed"])
         .from("todos")
-        .and_where(Expr::col("id").eq(todo_id.as_str()))
+        .and_where(Expr::col("id").eq(todo_id.to_string()).into())
         .build_rusqlite(SqliteQueryBuilder);
 
     let mut stmt = conn.prepare_cached(sql.as_str())?;
@@ -175,7 +175,9 @@ mod tests {
             title: "Updated".to_string(),
             completed: true,
         };
-        update_todo(&mut conn, &updated_todo).expect("Should be able to update todo");
+        let row_count =
+            update_todo(&mut conn, &updated_todo).expect("Should be able to update todo");
+        assert!(row_count > 0);
 
         let todos = read_todos(&mut conn).expect("Should be able to read todos");
         assert_eq!(todos.len(), 1);
